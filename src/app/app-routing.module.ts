f1467c260ea5350e44f27a07be948aeea235cd21
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-08-23 12:11:38 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GiphyComponent } from './giphy/giphy.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:GiphyComponent},
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path:"**", component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
