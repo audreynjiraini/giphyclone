e8bb71d517633b2f8268ac2e958e2f75d5477b60
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-08-23 16:03:41 +0300

    make API request using promise and console log results

--- src/app/services/giphy-http-service.service.ts
+++ src/app/services/giphy-http-service.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GiphyHttpServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   searchGiphies(searchTerm: string) {
     console.log(searchTerm);
+    let searchEndpoint = "api.giphy.com/v1/gifs/search" + environment.GIPHYAPIKEY;
+    searchEndpoint += "&q=" + searchTerm;
+
+    let promise = new Promise((resolve, reject)=>{
+      this.http.get(searchEndpoint).toPromise().then(
+        (results)=>{
+          console.log(results);
+          resolve()
+        },
+        (error)=>{
+          console.log(error)
+          reject()
+        }
+        )
+    })
+    return promise;
   }
 }
